e989337e158375e2ebcf41b1b1aad7eaad55f463
Tobias Llop <tobiasllop@gmail.com>
2025-06-17 18:03:23 -0300

    Update p6.R

--- p6.R
+++ p6.R
@@ -83,3 +83,93 @@ predict(modelo, newdata = data.frame(W = W_nuevo), interval = "prediction", leve
 #i
 intervalo_log <- predict(modelo, newdata = data.frame(W = W_nuevo), interval = "prediction", level = 0.95)
 exp(intervalo_log)
+
+
+#####################################################################
+##### Ejercicio 5 ###################################################
+#####################################################################
+
+#a
+#Cargar datos
+credit <- read.csv("~/Descargas/credit.txt", header = TRUE)
+str(credit)
+
+#Definimos variables indicadoras
+credit$GenderFemale <- as.integer(trimws(credit$Gender) == "Female")
+credit$StudentYes <- as.integer(credit$Student == "Yes")
+credit$MarriedYes <- as.integer(credit$Married == "Yes")
+str(credit)
+
+#Ajustar el modelo propuesto
+modelo_credit <- lm(Balance ~ Income + Rating + Limit + Cards + Age + Education +
+                      GenderFemale + StudentYes + MarriedYes,
+                    data = credit)
+summary(modelo_credit)
+
+
+#b
+# Valor predicho
+y_hat2 <- predict(modelo_credit)[2]
+
+# Residuo
+residuo2 <- residuals(modelo_credit)[2]
+
+# Suma total de residuos
+suma_residuos <- sum(residuals(modelo_credit))
+
+# Correlaciones con residuos
+cor_edu_res <- cor(credit$Education, residuals(modelo_credit))
+cor_lim_res <- cor(credit$Limit, residuals(modelo_credit))
+
+# Correlaciones con todas las variables explicativas
+cor_con_covariables <- sapply(credit[, c("Income", "Rating", "Limit", "Cards", "Age", "Education",
+                                         "GenderFemale", "StudentYes", "MarriedYes")],
+                              function(x) cor(x, residuals(modelo_credit)))
+
+# Mostrar resultados
+cat("Predicción para obs 2:", y_hat2, "\n")
+cat("Residuo para obs 2:", residuo2, "\n")
+cat("Suma total de residuos:", suma_residuos, "\n")
+cat("Correlación Education ~ residuos:", cor_edu_res, "\n")
+cat("Correlación Limit ~ residuos:", cor_lim_res, "\n")
+cat("Correlaciones con otras variables:\n")
+print(cor_con_covariables)
+
+
+#c
+sigma2 <- summary(modelo_credit)$sigma^2
+cat("Estimación de sigma²:", sigma2, "\n")
+
+#d
+coefs <- summary(modelo_credit)$coefficients
+significativas <- coefs[coefs[, "Pr(>|t|)"] < 0.05, ]
+cat("Variables estadísticamente significativas:\n")
+print(significativas)
+
+#d
+t_age <- coefs["Age", "t value"]
+p_bilateral_age <- coefs["Age", "Pr(>|t|)"]
+gl <- modelo_credit$df.residual
+p_unilateral_izq <- pt(t_age, df = gl)
+
+cat("p-valor bilateral para Age:", p_bilateral_age, "\n")
+cat("p-valor unilateral (cola izquierda):", p_unilateral_izq, "\n")
+
+#e
+confint(modelo_credit, level = 0.90)["Education", ]
+
+#f
+beta_age <- coefs["Age", "Estimate"]
+se_age <- coefs["Age", "Std. Error"]
+
+diferencia <- -3 * beta_age
+se_dif <- sqrt((3 * se_age)^2)
+t_crit <- qt(0.975, df = gl)
+
+ic_inf <- diferencia - t_crit * se_dif
+ic_sup <- diferencia + t_crit * se_dif
+
+cat("Diferencia esperada:", diferencia, "\n")
+cat("IC 95% de la diferencia: [", ic_inf, ",", ic_sup, "]\n")
+
+
